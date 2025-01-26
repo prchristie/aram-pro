2e099005cc9ec90223743e6dfbd0e18229a1563f
Patrick Christie <patrick.christie.dev@gmail.com>
2025-01-26 19:42:42 +1100

    Move the build page into its own file

---
+++ frontend/src/components/pages/BuildPage.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { useChampion, useChampionBuilds } from "../../services/lol/lol";
+import { Build } from "../../types/build.types";
+import { BuildCarousel } from "../feature/build/carousel/BuildCarousel";
+import { RuneDisplay } from "../feature/build/runes/RuneDisplay";
+import "./build-page.css";
+import { useParams } from "@tanstack/react-router";
+
+export function BuildPage() {
+  const params = useParams({ from: "/champion/$name" });
+  const champion = useChampion(params.name);
+  const builds = useChampionBuilds(params.name);
+  const [selectedBuild, setSelectedBuild] = useState<undefined | Build>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (builds.error || builds.isPending) {
+      return;
+    }
+
+    setSelectedBuild(builds.data[0]);
+  }, [builds.error, builds.isPending, builds.data]);
+
+  if (
+    champion.error ||
+    champion.isPending ||
+    builds.error ||
+    builds.isPending
+  ) {
+    return <>Woops</>;
+  }
+
+  return (
+    <div className="build-page">
+      <div className="build-page__build-carousel__container">
+        <img
+          className="build-page__champion-portrait"
+          src={champion.data.portraitUrl}
+          alt={`${champion.data.name}'s portrait art`}
+          width={128}
+          height={128}
+        />
+        <BuildCarousel
+          builds={builds.data}
+          onBuildSelected={setSelectedBuild}
+        />
+      </div>
+      <div>
+        <div className="build-display-container">
+          <div className="rune-display-container">
+            {selectedBuild ? (
+              <RuneDisplay
+                runes={selectedBuild.runes}
+                selectedKeystone={selectedBuild.keystone}
+              />
+            ) : (
+              "Loading"
+            )}
+          </div>
+          <div style={{ flex: 1 }}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/champion/$name.tsx
+++ frontend/src/routes/champion/$name.tsx
@@ -1,10 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useChampion, useChampionBuilds } from "../../services/lol/lol";
-import { BuildCarousel } from "../../components/feature/build/carousel/BuildCarousel";
-import "./build-page.css";
-import { RuneDisplay } from "../../components/feature/build/runes/RuneDisplay";
-import { useEffect, useState } from "react";
-import { Build } from "../../types/build.types";
+import { BuildPage } from "../../components/pages/BuildPage";
 
 export const Route = createFileRoute("/champion/$name")({
   component: () => {
@@ -12,66 +7,3 @@ export const Route = createFileRoute("/champion/$name")({
   },
   pendingComponent: () => <>loading</>,
 });
-
-function BuildPage() {
-  const params = Route.useParams();
-  const champion = useChampion(params.name);
-  const builds = useChampionBuilds(params.name);
-  const [selectedBuild, setSelectedBuild] = useState<undefined | Build>(
-    undefined
-  );
-
-  useEffect(() => {
-    if (builds.error || builds.isPending) {
-      return;
-    }
-
-    setSelectedBuild(builds.data[0]);
-  }, [builds.error, builds.isPending, builds.data]);
-
-  if (
-    champion.error ||
-    champion.isPending ||
-    builds.error ||
-    builds.isPending
-  ) {
-    return <>Woops</>;
-  }
-
-  return (
-    <div className="build-page">
-      <div className="build-page__build-carousel__container">
-        <img
-          className="build-page__champion-portrait"
-          src={champion.data.portraitUrl}
-          alt={`${champion.data.name}'s portrait art`}
-          width={128}
-          height={128}
-        />
-        <BuildCarousel
-          builds={builds.data}
-          onBuildSelected={setSelectedBuild}
-        />
-      </div>
-      <div>
-        <div
-          className="build-display-container"
-        >
-          <div
-            className="rune-display-container"
-          >
-            {selectedBuild ? (
-              <RuneDisplay
-                runes={selectedBuild.runes}
-                selectedKeystone={selectedBuild.keystone}
-              />
-            ) : (
-              "Loading"
-            )}
-          </div>
-          <div style={{ flex: 1 }}></div>
-        </div>
-      </div>
-    </div>
-  );
-}
